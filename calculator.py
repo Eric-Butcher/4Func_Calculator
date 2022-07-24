179932f1a60bcf34999fe6fd03636d58b6540930
Eric-Butcher <ebutcher66@gmail.com>
2022-07-24 15:06:13 -0400

    Added comments.

--- calculator.py
+++ calculator.py
@@ -80,7 +80,7 @@ button_add.grid(row=3, column=3, sticky=base_sticky)
 ## create a class that will be used to create a single object that will store the necessary info to operate the applications functinos
 
 # create the class
-class calculationInfo():
+class CalculationInfo():
     def __init__(self): # initialize a new object
         self.tb_format = "" # can either be: "", "single", or "double"
                             # "" means the tb is displaying nothing
@@ -99,7 +99,7 @@ class calculationInfo():
 
 
 # create the object
-info = calculationInfo()
+info = CalculationInfo()
 
 ## helper functions
 
@@ -137,6 +137,7 @@ def format_num(num):
         else:
             return num
 
+# checks if the calculation is too big for this calculator's UI to handle
 def check_too_long(num):
     numstring = str(num)
     if (len(numstring) >= 22):
@@ -144,9 +145,10 @@ def check_too_long(num):
     else:
         return False
 
-
-
 # calculates
+# if the operation performes a number that is too large than it
+# returns OVERFLOW which will brick the calculator with that error
+# else it will return the solution
 def calculate(operation, first_term, second_term):
     if (operation == "+"):
         the_sum = first_term + second_term
@@ -167,6 +169,7 @@ def calculate(operation, first_term, second_term):
         else:
             return the_product
     elif (operation == "÷"):
+        # check for divide by zero
         if(second_term == 0):
             return ("UNDEFINED")
         else:
@@ -196,16 +199,33 @@ def set_button_state(current_state):
     button_divide.config(state=current_state)
     button_equals.config(state=current_state)
 
+# when any of the number buttons are clicked
 def clicked_num(num):
+    # if the top box is already displaying a full calculation
+    # example: 9 + 9 =
+    # then clicking a number button will clear the top box and write that
+    # new number to the bottom box
     if (info.tb_format == "double"):
         clicked_clear()
         info.bb_value = num
         update_bb_text(str(num))
+    # anytime that clicking a number button will replace what is
+    # currently being shown in the bottom box
+    # example would be at start (or after clear) when the bottom box
+    # displays 0, or after the user has pressed 3 and x, top box will 
+    # be showing 3 x, but the bottom box will still be showing 3. 
+    # this allows the user to simply press the new number and have that
+    # 3 be replaced
     elif (info.bb_value == None or info.bb_value == 0): 
         info.bb_value = num
         update_bb_text(str(num))
+    # will not place more numbers into the input if the number is getting
+    # too long for this calculator to handle from a UI and accuracy perspective
     elif (  (len(bottom_box["text"])) >= 11):
         pass
+    # when numbers are being added to the numberstring input
+    # example: user already has 34 in bottom box, clicks 9, 
+    # bottom box now shows 349
     else:
         new_value = bottom_box["text"] + str(num)
         new_value = float(new_value)
@@ -216,14 +236,30 @@ def clicked_num(num):
 
 
 def clicked_operation(operation):
+
+    # when nothing is being displayed in the top box, 
+    # take the number in the bottom box and make it the
+    # left number in the top box with the operation chosen
+    # example: user pressed 5 +, top box now shows 5 +
     if (info.tb_format == ""):
         info.tb_l_num = info.bb_value
         info.bb_value = None
         format = "single"
         format_tb_text(format, operation)
+
+    # if the top box is currently showing something like 5 +
+    # and the user pressed an operation without hitting a number key to input a number,
+    # the operation will change
+    # example: starts at 5 +, user presses x, now 5 x
     elif (info.tb_format == "single" and info.bb_value == None):
         format = "single"
         format_tb_text(format, operation)
+
+    # if the top box is currently showing something like 5 + and the user
+    # pressed a number key to input a value, and they press an operation key, 
+    # perform that operation and return the result to the top box as the left number
+    # example: 5 + in top box, user pressed 3 so bottom box displays 3. After hitting the
+    # plus sign again the top box will display 8 + and 8 in the bottom box
     elif (info.tb_format == "single" and info.bb_value != None):
         solution = calculate(info.current_operation, info.tb_l_num, info.bb_value)
         if is_not_solution(solution):
@@ -235,19 +271,36 @@ def clicked_operation(operation):
 
             info.bb_value = None
             update_bb_text(str(solution))
+
+    # if the top box is currently displaying something like 5 + 3 =
+    # and the user presses an operation (let's say +), format the top
+    # box as 8 +
     elif (info.tb_format == "double"):
         info.tb_l_num = info.bb_value
         info.bb_value = None
         format_tb_text("single", operation)
 
 def clicked_equals():
+
+    # if there is nothing in the top box and the
+    # user presses =, take the value in the bottom box
+    # and put it as the top left number in the top box
+    # with the equal sign
     if (info.tb_format == ""):
         info.tb_l_num = info.bb_value
         info.bb_value = None
         format = "single"
         format_tb_text(format, "=")
+
+    # if the top box is already displaying something like 5 = 
+    # and the user presses equals again, do nothing
     elif (info.tb_format == "single" and info.current_operation == "="):
         pass
+
+    # if the top box is displaying something like 5 +, 
+    # perform that calculation with the number in the 
+    # bottom box and change the top box to show the previous 
+    # number in the bottom box and change the bottom box number to the result
     elif (info.tb_format == "single"):
         info.tb_r_num = float(bottom_box["text"])
         info.tb_r_num = format_num(info.tb_r_num)
@@ -259,6 +312,10 @@ def clicked_equals():
             solution = format_num(solution)
             update_bb_text(str(solution))
             info.bb_value = solution
+
+    # if the top box is already showing something like 5 + 5 =, 
+    # peform the operation and make the result the top left number in the
+    # top box
     elif (info.tb_format == "double"):
         info.tb_l_num = info.bb_value
         format_tb_text("double", info.current_operation)
@@ -277,9 +334,11 @@ def clicked_clear():
     update_bb_text("0")
     info.reset_variables()
 
-    # reset from an undefined answer
+    # reset from an undefined/overflow answer
     set_button_state("normal")
 
+# checks to see if the solution is 
+# a valid number solution
 def is_not_solution(solution):
     not_correct = ["UNDEFINED", "OVERFLOW"]
     if solution in not_correct:
@@ -287,11 +346,13 @@ def is_not_solution(solution):
     else:
         return False
 
+# function that disables calculator in case
+# of an OVERFLOW or UNDEFINED
 def cannot_calculate(message):
     update_tb_text("")
     update_bb_text(str(message))
     
-    # grey out all the buttons except clear
+    # grey out  and deactivate all the buttons except clear
     set_button_state("disabled")
     
 
